2c2243404026f1a64122fc1d073706585dadeb3b
Himanshu Pant <himanshu.pn3@gmail.com>
2018-01-23 10:36:59 -0800

    added multer to process form data in request

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
-
+const upload = multer();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static("client"));
