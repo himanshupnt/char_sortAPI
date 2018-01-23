c84679fc1319f25832e13a02af4cfee9e43591ed
Himanshu Pant <himanshu.pn3@gmail.com>
2018-01-23 10:40:26 -0800

    added sort() for incoming requests

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ const upload = multer();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static("client"));
-app.listen(PORT, () => console.log("server running"));
+app.post("/sort", upload.array(), (req, res) => {
+  const { unsorted } = req.body;
+  const sorted = unsorted
+    .split("")
+    .sort()
+    .join("");
+  res.send({ sorted });
+});
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
