6d86a77dbfff9fe5ccde4d7cc161436275e5e587
Himanshu Pant <himanshu.pn3@gmail.com>
2018-01-23 12:19:01 -0800

    fixed test for reversed string

--- sorter.test.js
+++ sorter.test.js
@@ -3,7 +3,7 @@ const { strSorter } = require("./sorter");
 describe("string sorter", () => {
   it("given an unsorted string, outputs a sorted string", () => {
     const unsorted = "aljfsafka3jsfadf454asfasflier98734";
-    const sorted = "334445789aaaaaadeffffffijjkllrssss";
+    const sorted = "ssssrllkjjiffffffedaaaaaa987544433";
     expect(strSorter(unsorted) === sorted).toBe(true);
   });
   it("ouputs an empty string, given an empty string as input", () => {
