049d80f86a4b014b958c2de797c842857a18a086
Himanshu Pant <himanshu.pn3@gmail.com>
2018-01-23 10:44:20 -0800

    added error handler

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const upload = multer();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static("client"));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("internal server error!");
+  next();
+});
 app.post("/sort", upload.array(), (req, res) => {
   const { unsorted } = req.body;
   const sorted = unsorted
